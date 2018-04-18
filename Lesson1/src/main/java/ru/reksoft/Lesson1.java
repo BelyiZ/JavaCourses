9892a9ccb25947f92c497eff264e3fa696ad7e27
belyiz <belyi.zurab@gmail.com>
2018-04-18 16:48:50 +0300

    delete new method

--- Lesson1/src/main/java/ru/reksoft/Lesson1.java
+++ Lesson1/src/main/java/ru/reksoft/Lesson1.java
@@ -4,10 +4,5 @@ public class Lesson1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(y(3));
-    }
-
-    public static int y(int x) {
-        return 2*x + 1;
     }
 }

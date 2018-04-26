9f181c4c123a5ce87727d07a2298c765c9f3b097
belyiz <belyi.zurab@gmail.com>
2018-04-26 22:55:13 +0300

    Lesson 2 homework

---
+++ Lesson1/src/test/java/ru/reksoft/Lesson2Test.java
@@ -0,0 +1,23 @@
+package ru.reksoft;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lesson2Test {
+
+    @Test
+    void formula() {
+        assertEquals(5, Lesson2.formula(1), "y(1) = 5");
+        assertEquals(13, Lesson2.formula(15), "y(15) = 13");
+        assertEquals(44, Lesson2.formula(155), "y(155) = 44");
+    }
+
+    @Test
+    void initials() {
+        assertEquals("И.И.П.", Lesson2.initials("Иванов Иван Петрович"), "Иванов Иван Петрович → И.И.П.");
+        assertEquals("П.А.Н.", Lesson2.initials("Петров алексей Никифорович"), "Петров алексей Никифорович → П.А.Н.");
+        assertEquals("К.Г.В.", Lesson2.initials("Козлова Галина Васильевна"), "Козлова Галина Васильевна → К.Г.В.");
+        assertEquals("П.А.С.", Lesson2.initials("Пушкин А. С."), "Пушкин А. С. → П.А.С.");
+    }
+}

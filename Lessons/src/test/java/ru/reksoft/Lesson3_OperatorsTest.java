7348f37c870392b314875e49e7784dfc8251e648
belyi <beliy@reksoft.ru>
2018-05-11 16:49:33 +0300

    Поправил тест для третьего задания

--- Lessons/src/test/java/ru/reksoft/Lesson3_OperatorsTest.java
+++ Lessons/src/test/java/ru/reksoft/Lesson3_OperatorsTest.java
@@ -15,17 +15,17 @@ class Lesson3_OperatorsTest {
 
     @Test
     void task1() {
-        Assert.assertEquals("1, abcde → abcde1", Lesson3_Operators.task1(1, "abcde"), "abcde1");
-        Assert.assertEquals("-1, abcde → -1abcde", Lesson3_Operators.task1(-1, "abcde"), "-1abcde");
-        Assert.assertEquals("11223344, abcde → abcde11223", Lesson3_Operators.task1(11223344, "abcde"), "abcde11223344");
-        Assert.assertEquals("-12345, abcde → -12345abcd", Lesson3_Operators.task1(-12345, "abcde"), "-12345abcde");
-        Assert.assertEquals("0, abcde → 0", Lesson3_Operators.task1(0, "abcde"), "0");
-        Assert.assertEquals("4, 123 → 1234", Lesson3_Operators.task1(4, "123"), "1234");
+        Assert.assertEquals("1, abcde → abcde1", "abcde1", Lesson3_Operators.task1(1, "abcde"));
+        Assert.assertEquals("-1, abcde → -1abcde", "-1abcde", Lesson3_Operators.task1(-1, "abcde"));
+        Assert.assertEquals("11223344, abcde → abcde11223", "abcde11223", Lesson3_Operators.task1(11223344, "abcde"));
+        Assert.assertEquals("-12345, abcde → -12345abcd", "-12345abcd", Lesson3_Operators.task1(-12345, "abcde"));
+        Assert.assertEquals("0, abcde → 0", "0", Lesson3_Operators.task1(0, "abcde"));
+        Assert.assertEquals("4, 123 → 1234", "1234", Lesson3_Operators.task1(4, "123"));
     }
 
     @Test
     void task2() {
-        Assert.assertTrue("45 → true", Lesson3_Operators.task2(45));
+        Assert.assertEquals("45 → true", true, Lesson3_Operators.task2(45));
         Assert.assertTrue("75 → true", Lesson3_Operators.task2(75));
         Assert.assertTrue("99 → true", Lesson3_Operators.task2(99));
         Assert.assertTrue("31 → true", Lesson3_Operators.task2(31));

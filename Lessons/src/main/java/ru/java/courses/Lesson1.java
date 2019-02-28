8721bb4d02f782e8e2ead8a8191b034555976380
belyiz <belyi.zurab@gmail.com>
2019-02-28 17:56:55 +0300

    Hello world -> Hello, Zurab

--- Lessons/src/main/java/ru/java/courses/Lesson1.java
+++ Lessons/src/main/java/ru/java/courses/Lesson1.java
@@ -3,6 +3,6 @@ package ru.java.courses;
 public class Lesson1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, Zurab!");
     }
 }

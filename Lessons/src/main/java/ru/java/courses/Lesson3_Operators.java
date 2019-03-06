525eaba9b28f70d1d2c0ef0ef9c2ff97399eb68b
verolukina@gmail.com <Ehnaton18!>
2019-03-06 16:40:39 +0300

    Answers for Lesson3

--- Lessons/src/main/java/ru/java/courses/Lesson3_Operators.java
+++ Lessons/src/main/java/ru/java/courses/Lesson3_Operators.java
@@ -18,9 +18,19 @@ public class Lesson3_Operators {
      * - получить первые n символов из строки можно методом .substring(0, n)
      */
     public static String task1(int i, String str) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
-        return null;
+        if (i > 0) {
+            str += i;
+        } else if (i < 0) {
+            str = i + str;
+        } else {
+            str = Integer.toString(i);
+        }
+
+        if (str.length() <= 10) {
+            return str;
+        } else {
+            return str.substring(0, 10);
+        }
     }
 
     /**
@@ -37,10 +47,17 @@ public class Lesson3_Operators {
      * Подсказка: тут все просто, думайте сами ;)
      */
     public static boolean task2(int i) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
-        return false;
+        i -= 1;
+        if (i > 100 || i < 30) {
+            i += 1000;
+        } else if (i > 0 && i % 2 == 0) {
+            i -= 1000;
+        }
+
+        if (i < 0){
+            return false;
+        } else {
+            return true;
+        }
     }
-
-
 }

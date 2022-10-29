5ee93ab0b1c7584f5bd915f536b44b4ee0767d20
Arthur Vartanyan <rogue.2019@icloud.com>
2022-10-29 08:50:45 +0300

    Дописал ТЗ по заданию

--- ReflectionAPI/src/main/java/com/homework/reflection/api/dto/UserTableMetadata.java
+++ ReflectionAPI/src/main/java/com/homework/reflection/api/dto/UserTableMetadata.java
@@ -1,4 +1,14 @@
 package com.homework.reflection.api.dto;
 
+/**
+ * UserTableMetadata class.
+ * Важно: необхоидмо дописать класс, исходя из задания!
+ * <p>
+ * Требования для создания класса UserTableMetadata:
+ * а) публичный класс
+ * б) имеет 2 поля: tableName - String(название таблицы),
+ * fieldWithType - Map<String, String>(название колонки и соответствующий ему тип).
+ * в) поля должны быть с модификатором private, при этом должны иметь соответствующие getter/setter методы.
+ */
 public class UserTableMetadata {
 }
